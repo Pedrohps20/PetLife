716842f45dd00ec13729616ad40c6439cb2989bd
Pedro Henrique <154438597+Pedrohps20@users.noreply.github.com>
2024-01-10 21:25:37 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+var elementosDuvida = document.querySelectorAll('.duvida')
+elementosDuvida.forEach(function(duvida){
+    duvida.addEventListener('click', function () {
+        duvida.classList.toggle('ativa')
+    })
+})
